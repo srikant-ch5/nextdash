4aefdbb6de0acebba0a7cf4b236d49f6a4e7434d
srikant <srksriks123@gmail.com>
2025-09-26 21:15:13 +0530

    Intial setup commit

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ELntOHJRyBQia0ML+sDUuCssN4Ff4J01ykL93vaHxLY=\"\n}"
---
+++ .next/static/chunks/app_layout_tsx_be0f615f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__3314af62._.css",
+  "static/chunks/f07ee_next_dist_35a4b6e8._.js"
+],
+    source: "dynamic"
+});
